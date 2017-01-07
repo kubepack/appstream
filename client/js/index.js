78bb98d74e8979a72ceeeb5497849ee267f44f51
tamal <tamal@appscode.com>
2017-01-07 05:32:05 -0800

    Regenerate JS client

--- client/js/index.js
+++ client/js/index.js
@@ -8,6 +8,7 @@ This is a RSVP based Ajax client for AppsCode Seed gRPC JSON APIs.
 var _ = require('lodash');
 
 var apis = _.merge({},
-    require('./apis/app/v1beta1/app.gw.js')
+    require('./apis/app/v1beta1/metdata.gw.js'),
+    require('./apis/app/v1beta1/version.gw.js')
 );
 module.exports = apis.appscode;
